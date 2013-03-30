6a123b37377e7789b01cd9bdac3b87ed11df7dc5
Schrws <suzynjun99@naver.com>
2013-03-30 19:01:13 +0900

    add cleartype custom apps

--- common.mk
+++ common.mk
@@ -233,6 +233,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
   ro.cm.version=$(CM_VERSION) \
   ro.modversion=$(CM_VERSION)
 
+PRODUCT_COPY_FILES += \
+    vendor/cleartype/app/info.apk:system/app/t.cleartype.info-1.apk \
+    vendor/cleartype/app/preferences:system/app/com.cleartype.preferences-1.apk
+
 # Copy over the changelog to the device # CLEARTYPE
 PRODUCT_COPY_FILES += \
     vendor/cleartype/CHANGELOG.mkdn:system/etc/CHANGELOG-CLEARTYPE.txt
